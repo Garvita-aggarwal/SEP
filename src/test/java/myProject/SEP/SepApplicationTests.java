bfe306921b6245f56db1395c700a5e200bb17150
Garvita-aggarwal <garvitaa18@gmail.com>
2024-06-09 17:44:23 +0530

    first commit

---
+++ src/test/java/myProject/SEP/SepApplicationTests.java
@@ -0,0 +1,11 @@
+package myProject.SEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SepApplicationTests {
+	public static void main(String[] args) {
+		System.out.println("Hello project");
+	}
+}
